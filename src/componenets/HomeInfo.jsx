8f5f979d1f3c2a8751871a19b55f53eccd5e54b5
Sahil Singh <114878612+sahilsingh12221802@users.noreply.github.com>
2025-08-18 20:46:28 +0530

    Update HomeInfo.jsx

--- src/componenets/HomeInfo.jsx
+++ src/componenets/HomeInfo.jsx
@@ -17,7 +17,7 @@ const renderContent = {
         <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">
             Hi, I am <span className="font-semibold"> Sahil Singh</span>👋
             <br />
-            A B.Tech 3rd Year Student From India
+            A B.Tech 4th Year Student From India
         </h1>
     ),
     2: (
